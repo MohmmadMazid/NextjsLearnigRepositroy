18edf35506b2646e24354cf84a4043efcb187648
MohmmadMazid <mazidmansury5500@gmail.com>
2025-09-22 17:45:03 +0530

    used conditional layouting

--- src/app/contact/layout.jsx
+++ src/app/contact/layout.jsx
@@ -1,9 +1,19 @@
+"use client";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const ContactCommmanLayout = ({ children }) => {
+  // we are going to use common layout as a conditional layout because we just want to show this layout all the routes
+  //except one route.
+  // for achieving this we will be using the next hook which is (usePathName()) for getting the current route name
+
+  const currentPathName = usePathname();
+  console.log(currentPathName);
   return (
     <div>
-      <h1>Hello This is Our Common Layout Page</h1>
+      {currentPathName !== "/contact/empContact" ? (
+        <h1>Hello This is Our Common Layout Page</h1>
+      ) : null}
       {/* use the childeren object property because it help to render the all the child component of this route */}
       {children}
     </div>
