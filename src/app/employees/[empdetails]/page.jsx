ccec7f100247ab6ad4c4778923fa7cbdae4226c3
MohmmadMazid <mazidmansury5500@gmail.com>
2025-09-23 12:20:29 +0530

    second way of getting dynamic routes

--- src/app/employees/[empdetails]/page.jsx
+++ src/app/employees/[empdetails]/page.jsx
@@ -1,19 +1,41 @@
-// "use client";
+"use client";
+import { useParams } from "next/navigation";
 import React from "react";
 
-const EmployeeDetials = async ({ params }) => {
-  console.log(await params);
-  let name = await params.empdetails;
-  // whatever data is comming from the params means as a dynamic routing params we will get all of them
-  // using params which is inbuilt next js feature
+// second way of dynamic routing
+
+// remember one thing dynamic folder name and its functional component Name should be the same
+// if both are not same then you will find the error of name conflicting
+const empdetails = () => {
+  const params = useParams();
+  console.log(params.empdetails);
+  //   let name = await params.empdetails;
+
   return (
     <div>
       <div>
         <h1>employee details is given below</h1>
-        <h2>Employee Name :{name}</h2>
+        <h2>Employee Name :{params.empdetails}</h2>
       </div>
     </div>
   );
 };
 
-export default EmployeeDetials;
+// first way getting dynamic pararms
+
+// const EmployeeDetials = async ({ params }) => {
+//   console.log(await params);
+//   let name = await params.empdetails;
+//   // whatever data is comming from the params means as a dynamic routing params we will get all of them
+//   // using params which is inbuilt next js feature
+//   return (
+//     <div>
+//       <div>
+//         <h1>employee details is given below</h1>
+//         <h2>Employee Name :{name}</h2>
+//       </div>
+//     </div>
+//   );
+// };
+
+export default empdetails;
