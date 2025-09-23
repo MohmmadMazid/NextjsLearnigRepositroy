1dedcee7853ec706d37a1451de56fbdbb87172e0
MohmmadMazid <mazidmansury5500@gmail.com>
2025-09-23 19:53:46 +0530

    clinet side data fetching

---
+++ src/app/users/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+// this is the client side data fetching that is why we have use "use client " directives
+
+import React, { useEffect, useState } from "react";
+
+const UsersData = () => {
+  let [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getingUserData = async () => {
+      let response = await fetch("https://jsonplaceholder.typicode.com/users");
+      let data = await response.json();
+      //   console.log(data);
+      setUsers(data);
+    };
+    getingUserData();
+    // console.log(users);
+  }, []);
+
+  // i have used the useEffect hook for the data fetching on the time mounting  of the component on the browsers
+  return (
+    <div>
+      <div>
+        <h1>Fetching Users Data In The Client Side Component</h1>
+      </div>
+      <ol>
+        {users.map((el) => {
+          return (
+            <li key={el.id}>
+              <h3>{el.name}</h3>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+};
+
+export default UsersData;
