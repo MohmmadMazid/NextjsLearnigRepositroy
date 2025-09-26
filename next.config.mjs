a1c7910e18f1307cea6fd4c583b24e6a3972b608
MohmmadMazid <mazidmansury5500@gmail.com>
2025-09-26 20:53:06 +0530

    Image Optimazation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"], // add the domain from which you are getting the image as url
+    // and now only this domain image will be shown on the browser ui
+  },
+};
 
 export default nextConfig;
