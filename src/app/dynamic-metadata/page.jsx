a8eb006599be83b20672d7c4f80d4c84be41f6b2
MohmmadMazid <mazidmansury5500@gmail.com>
2025-09-27 20:42:21 +0530

    some changes

--- src/app/dynamic-metadata/page.jsx
+++ src/app/dynamic-metadata/page.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const DynamicMetaData = () => {
   return (
     <div>
-      <title>Your Dynamic Title</title>{" "}
+      {/* <title>Your Dynamic Title</title>{" "} */}
       {/* This is the bad practice for making dynamic title and description it makes  the extra title tag in your head section */}
       <div>
         <h1>Generate Dynamic Metad Data For Search Engine Optimization</h1>
